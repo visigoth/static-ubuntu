b24b2bd6737b507d565b80a64a1f79d1db274684
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-16 20:45:47 +0000

    Update install-sonarr.sh

--- scripts-install/install-sonarr.sh
+++ scripts-install/install-sonarr.sh
@@ -2,13 +2,13 @@
 
 ## Install dependencies ##
 apt -y update \
-    && apt -y install --no-install-recommends --no-install-suggests libicu66 libmediainfo0v5 sqlite3
+    && apt -y install libicu66 libmediainfo0v5 sqlite3
 
 ## Install sonarr ##
 SONARR_BRANCH="main"
 SONARR_VERSION=$(curl -sX GET http://services.sonarr.tv/v1/releases | jq -r ".[] | select(.branch==\"${SONARR_BRANCH}\") | .version")
 rm -rf /app/sonarr/bin \
-    && curl -o /tmp/sonarr.tar.gz -L "https://download.sonarr.tv/v${SONARR_VERSION:0:1}/${SONARR_BRANCH}/${SONARR_VERSION}/Sonarr.${SONARR_BRANCH}.${SONARR_VERSION}.linux.tar.gz" \
+    && curl -s -o /tmp/sonarr.tar.gz -L "https://download.sonarr.tv/v${SONARR_VERSION:0:1}/${SONARR_BRANCH}/${SONARR_VERSION}/Sonarr.${SONARR_BRANCH}.${SONARR_VERSION}.linux.tar.gz" \
     && mkdir -p /app/sonarr/bin \
     && tar xf /tmp/sonarr.tar.gz -C /app/sonarr/bin --strip-components=1 \
     && rm -f /tmp/sonarr.tar.gz
