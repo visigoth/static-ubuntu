eaf13af176177a0c97c46486dde0474d874dc193
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-16 23:27:28 +0000

    Update install-openvpn-the-one-docker-base.sh

--- scripts-install/install-openvpn-the-one-docker-base.sh
+++ scripts-install/install-openvpn-the-one-docker-base.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 ## Run required install scripts ##
-#source /testdasi/scripts-install/install-flood-transmission.sh
-#source /testdasi/scripts-install/install-jackett.sh
-#source /testdasi/scripts-install/install-prowlarr.sh
-#source /testdasi/scripts-install/install-radarr.sh
-#source /testdasi/scripts-install/install-sonarr.sh
+source /testdasi/scripts-install/install-flood-transmission.sh
+source /testdasi/scripts-install/install-jackett.sh
+source /testdasi/scripts-install/install-prowlarr.sh
+source /testdasi/scripts-install/install-radarr.sh
+source /testdasi/scripts-install/install-sonarr.sh
+source /testdasi/scripts-install/install-nzbhydra2.sh
+# always run sab last due to a lot of dependencies #
+source /testdasi/scripts-install/install-sabnzbdplus.sh
+#source /testdasi/scripts-install/
