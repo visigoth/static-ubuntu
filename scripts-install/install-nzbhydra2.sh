6348d3112defa6c761c0b7559b189cb9b41cd544
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-16 22:33:34 +0000

    Update install-nzbhydra2.sh

--- scripts-install/install-nzbhydra2.sh
+++ scripts-install/install-nzbhydra2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+## create man1 folder otherwise openjdk-11-jre-headless would fail because of reasons ##
+#mkdir -p /usr/share/man/man1
+
 ## Install dependencies ##
 apt -y update \
     && apt -y install openjdk-11-jre-headless python3
@@ -7,7 +10,7 @@ apt -y update \
 ## Install nzbhydra2 ##
 NZBHYDRA2_VERSION=$(curl -sX GET "https://api.github.com/repos/theotherp/nzbhydra2/releases/latest" | jq -r .tag_name | cut -d'v' -f 2)
 rm -rf /app/nzbhydra2/bin \
-    && curl -L "https://github.com/theotherp/nzbhydra2/releases/download/v${NZBHYDRA2_VERSION}/nzbhydra2-${NZBHYDRA2_VERSION}-linux.zip" -o /tmp/nzbhydra2.zip \
+    && curl -sL "https://github.com/theotherp/nzbhydra2/releases/download/v${NZBHYDRA2_VERSION}/nzbhydra2-${NZBHYDRA2_VERSION}-linux.zip" -o /tmp/nzbhydra2.zip \
     && mkdir -p /app/nzbhydra2/bin \
     && unzip /tmp/nzbhydra2.zip -d /app/nzbhydra2/bin \
     && chmod +x /app/nzbhydra2/bin/nzbhydra2wrapperPy3.py \
