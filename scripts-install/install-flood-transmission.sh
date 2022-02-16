237daf2311187c7ca7e1a96940ddbbf9e203de6c
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-16 19:16:32 +0000

    Update install-flood-transmission.sh

--- scripts-install/install-flood-transmission.sh
+++ scripts-install/install-flood-transmission.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## Install dependencies ##
-apt-get -y update \
+apt -y update \
     && curl -sL https://deb.nodesource.com/setup_lts.x | bash - \
-    && apt-get -y install -y nodejs
+    && apt -y install -y nodejs
 
 ## Install torrent client ##
-apt-get -y install transmission-daemon
+apt -y install transmission-daemon
 TRANSMISSION_VERSION=$(dpkg -s transmission-daemon | grep 'Version' | cut -d' ' -f 2)
 
 ## New fork of flood at https://github.com/jesec/flood ##
@@ -14,10 +14,10 @@ npm install --global flood
 FLOOD_VERSION=$(npm list -g | grep 'flood' | cut -d'@' -f 2)
 
 ## Set build info ##
-echo "$(date "+%d.%m.%Y %T") Added flood (v${FLOOD_VERSION}) with transmission-daemon (v${TRANSMISSION_VERSION})" >> /build.info
+echo "$(date "+%d.%m.%Y %T") Added flood version ${FLOOD_VERSION} with transmission-daemon version ${TRANSMISSION_VERSION}" >> /build.info
 
 ## Clean up ##
-apt-get -y autoremove \
-    && apt-get -y autoclean \
-    && apt-get -y clean \
+apt -y autoremove \
+    && apt -y autoclean \
+    && apt -y clean \
     && rm -rf /tmp/* /var/tmp/* /var/lib/apt/lists/*
