3faecbd4b21c6796e502aeda1e52dfae776e5aa5
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-16 22:27:36 +0000

    Create install-nzbhydra2.sh

---
+++ scripts-install/install-nzbhydra2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## Install dependencies ##
+apt -y update \
+    && apt -y install openjdk-11-jre-headless python3
+
+## Install nzbhydra2 ##
+NZBHYDRA2_VERSION=$(curl -sX GET "https://api.github.com/repos/theotherp/nzbhydra2/releases/latest" | jq -r .tag_name | cut -d'v' -f 2)
+rm -rf /app/nzbhydra2/bin \
+    && curl -L "https://github.com/theotherp/nzbhydra2/releases/download/v${NZBHYDRA2_VERSION}/nzbhydra2-${NZBHYDRA2_VERSION}-linux.zip" -o /tmp/nzbhydra2.zip \
+    && mkdir -p /app/nzbhydra2/bin \
+    && unzip /tmp/nzbhydra2.zip -d /app/nzbhydra2/bin \
+    && chmod +x /app/nzbhydra2/bin/nzbhydra2wrapperPy3.py \
+    && chmod +x /app/nzbhydra2/bin/nzbhydra2 \
+    && rm -f /tmp/nzbhydra2.zip
+
+## Set package info and build info ##
+echo "ReleaseType=Release\nPackageVersion=${NZBHYDRA2_VERSION}\nPackageAuthor=[testdasi](https://github.com/testdasi)" > /app/nzbhydra2/package_info
+echo "$(date "+%d.%m.%Y %T") Added nzbhydra2 binary version ${NZBHYDRA2_VERSION}" >> /build.info
+
+## Clean up ##
+apt -y autoremove \
+    && apt -y autoclean \
+    && apt -y clean \
+    && rm -rf /tmp/* /var/tmp/* /var/lib/apt/lists/*
