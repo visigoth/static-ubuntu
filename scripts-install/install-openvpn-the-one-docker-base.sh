177204f810128cd0f187c01099e87685d9bebf6e
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-16 19:25:23 +0000

    Update install-openvpn-the-one-docker-base.sh

--- scripts-install/install-openvpn-the-one-docker-base.sh
+++ scripts-install/install-openvpn-the-one-docker-base.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo 'placeholder'
+## Run required install scripts ##
+source /testdasi/scripts-install/install-flood-transmission.sh
+source /testdasi/scripts-install/install-jackett.sh
+source /testdasi/scripts-install/install-prowlarr.sh
+source /testdasi/scripts-install/install-radarr.sh
+source /testdasi/scripts-install/install-sonarr.sh
