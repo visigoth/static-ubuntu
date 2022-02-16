6f0ed1f25ce8c7338b937772798d83a08d022b05
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-16 16:44:39 +0000

    Create reinstall-static-ubuntu.sh

---
+++ scripts-debug/reinstall-static-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Redownload scripts from github for further testing ##
+rm -Rf /testdasi
+mkdir -p /temp \
+    && cd /temp \
+    && curl -L "https://github.com/testdasi/static-ubuntu/archive/main.zip" -o /temp/temp.zip \
+    && unzip /temp/temp.zip \
+    && rm -f /temp/temp.zip \
+    && mv /temp/static-ubuntu-main /testdasi
