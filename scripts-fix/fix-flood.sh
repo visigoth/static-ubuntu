64480357755e35d20286564e69540a5f790d26fa
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 12:36:44 +0000

    Update fix-flood.sh

--- scripts-fix/fix-flood.sh
+++ scripts-fix/fix-flood.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+FLOOD_IP=${SERVER_IP}
+FLOOD_PORT=${TORRENT_GUI_PORT}
+sed -i "s|:3000|:$FLOOD_PORT|g" '/app/launcher/index.html'
+
 mkdir -p /config/flood/db \
     && cp -n /static-ubuntu/etc/flood.db /config/flood/db/users.db
 echo '[info] flood fixed.'
