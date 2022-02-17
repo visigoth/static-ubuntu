2973193c9a1617a53e25e86f80558d34a06c9522
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 21:30:35 +0000

    Create install-openvpn-plus.sh

---
+++ scripts-install/install-openvpn-plus.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Install dependencies ##
+apt -y update \
+    && apt -y install dnsutils sipcalc
+
+## install openvpn and nftables
+apt install -y openvpn nftables
+
+# install other apps
+apt -y install stubby dante-server tinyproxy
+
+# add tor and privoxy depending on torless tag
+if [[ ${BUILD_OPT} =~ "torless" ]]
+then
+    echo "$(date "+%d.%m.%Y %T") Skip torsocks and privoxy due to build option ${BUILD_OPT}" >> /build.info
+else
+    apt -y install torsocks privoxy    
+    echo "$(date "+%d.%m.%Y %T") Skip torsocks and privoxy due to build option ${BUILD_OPT}" >> /build.info
+fi
+
+
+# Clean up
+apt -y autoremove \
+    && apt -y autoclean \
+    && apt -y clean \
+    && rm -fr /tmp/* /var/tmp/* /var/lib/apt/lists/*
