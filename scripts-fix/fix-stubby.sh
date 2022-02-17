216bed28a59d575174d70f5a0027b78f4193bd60
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 14:29:30 +0000

    Update fix-stubby.sh

--- scripts-fix/fix-stubby.sh
+++ scripts-fix/fix-stubby.sh
@@ -4,7 +4,6 @@ DNS_PORT=${DNS_SERVER_PORT}
 # DoT port is fixed due to TLS protocol
 DOT_PORT=853
 sed -i "s|_DNS_PORT_|$DNS_PORT|g" '/nftables.rules'
-sed -i "s|_DANTE_PORT_|$DANTE_PORT|g" '/nftables.rules'
 
 mkdir -p /config/stubby \
     && cp -n /static-ubuntu/etc/stubby.yml /config/stubby/
