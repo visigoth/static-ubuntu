3fb8e4e969c75b683a85d361a7cf6f5c4546c662
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 21:45:36 +0000

    Update install-openvpn-plus.sh

--- scripts-install/install-openvpn-plus.sh
+++ scripts-install/install-openvpn-plus.sh
@@ -4,19 +4,45 @@
 apt -y update \
     && apt -y install dnsutils sipcalc
 
-## install openvpn and nftables
-apt install -y openvpn nftables
+## openvpn ##
+apt -y install openvpn
+OPENVPN_VERSION=$(openvpn --version | grep 'linux-gnu' | cut -d' ' -f 2)
+echo "$(date "+%d.%m.%Y %T") Added openvpn version ${OPENVPN_VERSION}" >> /build.info
 
-# install other apps
-apt -y install stubby dante-server tinyproxy
+## nftables ##
+apt -y install nftables
+NFT_VERSION=$(nft --version | cut -d' ' -f 2 | cut -d'v' -f 2)
+echo "$(date "+%d.%m.%Y %T") Added nftables version ${NFT_VERSION}" >> /build.info
 
-# add tor and privoxy depending on torless tag
+## stubby ##
+apt -y install stubby
+STUBBY_VERSION=$(stubby -V)
+echo "$(date "+%d.%m.%Y %T") Added stubby version ${STUBBY_VERSION}" >> /build.info
+
+## dante-server ##
+apt -y install dante-server
+DANTED_VERSION=$(danted -v | cut -d' ' -f 2 | cut -d'v' -f 2 | cut -c'1-5')
+echo "$(date "+%d.%m.%Y %T") Added dante-server version ${DANTED_VERSION}" >> /build.info
+
+## tinyproxy ##
+apt -y install tinyproxy
+TINYPROXY_VERSION=$(tinyproxy -v | cut -d' ' -f 2)
+echo "$(date "+%d.%m.%Y %T") Added tinyproxy version ${TINYPROXY_VERSION}" >> /build.info
+
+## add tor and privoxy depending on torless tag ##
 if [[ ${BUILD_OPT} =~ "torless" ]]
 then
     echo "$(date "+%d.%m.%Y %T") Skip torsocks and privoxy due to build option ${BUILD_OPT}" >> /build.info
 else
-    apt -y install torsocks privoxy    
-    echo "$(date "+%d.%m.%Y %T") Skip torsocks and privoxy due to build option ${BUILD_OPT}" >> /build.info
+    # torsocks #
+    apt -y install torsocks
+    TOR_VERSION=$(tor --version | cut -d' ' -f 3 | cut -c'1-7')
+    echo "$(date "+%d.%m.%Y %T") Added torsocks version ${TOR_VERSION}" >> /build.info
+    
+    # privoxy #
+    apt -y install privoxy
+    PRIVOXY_VERSION=$(privoxy --version | cut -d' ' -f 3)
+    echo "$(date "+%d.%m.%Y %T") Added privoxy version ${PRIVOXY_VERSION}" >> /build.info
 fi
 
 # Clean up
