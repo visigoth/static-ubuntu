e701a0bd1bb3d5b0e18a38f3eb3b74ff47a364b0
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 12:37:57 +0000

    Update fix-nzbhydra2.sh

--- scripts-fix/fix-nzbhydra2.sh
+++ scripts-fix/fix-nzbhydra2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+HYDRA_PORT=${SEARCHER_GUI_PORT}
+sed -i "s|:5076|:$HYDRA_PORT|g" '/app/launcher/index.html'
+
 mkdir -p /config/nzbhydra2 \
     && cp -n /static-ubuntu/etc/nzbhydra.yml /config/nzbhydra2/
 sed -i "s|port: 5076|port: $HYDRA_PORT|g" '/config/nzbhydra2/nzbhydra.yml'
