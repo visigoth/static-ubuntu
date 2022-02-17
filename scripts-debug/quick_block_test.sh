7ad2227da496e2a3c38813e7c38b0a0b3b2af5cb
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 11:16:37 +0000

    Create quick_block_test.sh

---
+++ scripts-debug/quick_block_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+### Quick block test ####
+ipttest=$(dig +short +time=5 +tries=1 myip.opendns.com @208.67.222.222)
+echo "[info] Quick block test. Expected result is time out. Actual result is $ipttest"
