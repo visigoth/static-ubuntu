3dfe62e6288936f541f6a45fc2cc64d70d7a27db
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 12:40:14 +0000

    Update fix-stubby.sh

--- scripts-fix/fix-stubby.sh
+++ scripts-fix/fix-stubby.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+DNS_PORT=${DNS_SERVER_PORT}
+# DoT port is fixed due to TLS protocol
+DOT_PORT=853
+
 mkdir -p /config/stubby \
     && cp -n /static-ubuntu/etc/stubby.yml /config/stubby/
 sed -i "s|  - 0\.0\.0\.0\@53|  - 0\.0\.0\.0\@$DNS_PORT|g" '/config/stubby/stubby.yml'
