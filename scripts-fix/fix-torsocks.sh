7a9ad3737843320d50f05c3e1b51e936a982865d
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 23:07:31 +0000

    Update fix-torsocks.sh

--- scripts-fix/fix-torsocks.sh
+++ scripts-fix/fix-torsocks.sh
@@ -5,5 +5,5 @@ sed -i "s|_TORSOCKS_PORT_|$TORSOCKS_PORT|g" '/nftables.rules'
 
 mkdir -p /config/tor \
     && cp -n /static-ubuntu/etc/torrc /config/tor/
-sed -i "s|SOCKSPort 0\.0\.0\.0:9050|SOCKSPort 0\.0\.0\.0:$TORSOCKS_PORT|g" '/config/tor/torrc'
+sed -i "s|\.0:9050|\.0:$TORSOCKS_PORT|g" '/config/tor/torrc'
 echo '[info] torsocks fixed.'
