f428b552c123856ef13dcc319991fb702e8955f6
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 17:48:34 +0000

    Update nftables.sh

--- scripts-openvpn/nftables.sh
+++ scripts-openvpn/nftables.sh
@@ -1,40 +1,5 @@
 #!/bin/bash
 
-### Editing ruleset ###
-echo '[info] Editing base ruleset'
-rm -f /nftables.rules
-cp /ruleset.nft /nftables.rules
-sed -i "s|_ETH0_NET_|$ETH0_NET|g" '/nftables.rules'
-sed -i "s|_HOST_NETWORK_|${HOST_NETWORK}|g" '/nftables.rules'
-sed -i "s|_OPENVPN_PROTO_|$OPENVPN_PROTO|g" '/nftables.rules'
-sed -i "s|_OPENVPN_PORT_|$OPENVPN_PORT|g" '/nftables.rules'
-sed -i "s|_DNS_PORT_|$DNS_PORT|g" '/nftables.rules'
-sed -i "s|_DANTE_PORT_|$DANTE_PORT|g" '/nftables.rules'
-sed -i "s|_TINYPROXY_PORT_|$TINYPROXY_PORT|g" '/nftables.rules'
-sed -i "s|_TORSOCKS_PORT_|$TORSOCKS_PORT|g" '/nftables.rules'
-sed -i "s|_PRIVOXY_PORT_|$PRIVOXY_PORT|g" '/nftables.rules'
-sed -i "s|_SAB_PORT_A_|$SAB_PORT_A|g" '/nftables.rules'
-sed -i "s|_SAB_PORT_B_|$SAB_PORT_B|g" '/nftables.rules'
-sed -i "s|_HYDRA_PORT_|$HYDRA_PORT|g" '/nftables.rules'
-sed -i "s|_FLOOD_PORT_|$FLOOD_PORT|g" '/nftables.rules'
-sed -i "s|_SONARR_PORT_|$SONARR_PORT|g" '/nftables.rules'
-sed -i "s|_RADARR_PORT_|$RADARR_PORT|g" '/nftables.rules'
-sed -i "s|_JACKETT_PORT_|$JACKETT_PORT|g" '/nftables.rules'
-sed -i "s|_PROWLARR_PORT_|$PROWLARR_PORT|g" '/nftables.rules'
-
-## Run TOR+Privoxy depending on build ##
-if [[ -f "/usr/sbin/tor" ]]; then
-    echo '[info] Tor build detected. Editing tor + privoxy ruleset'
-    sed -i "s|_TORSOCKS_PORT_|$TORSOCKS_PORT|g" '/nftables.rules'
-    sed -i "s|_PRIVOXY_PORT_|$PRIVOXY_PORT|g" '/nftables.rules'
-else
-    echo '[info] Torless build detected. Removing tor + privoxy ruleset'
-    sed -i 's|add rule ip filter INPUT iifname "eth0" tcp dport _TORSOCKS_PORT_ ct state new,established counter accept||g' '/nftables.rules'
-    sed -i 's|add rule ip filter INPUT iifname "eth0" tcp dport _PRIVOXY_PORT_ ct state new,established counter accept||g' '/nftables.rules'
-    sed -i 's|add rule ip filter OUTPUT oifname "eth0" tcp sport _TORSOCKS_PORT_ ct state established counter accept||g' '/nftables.rules'
-    sed -i 's|add rule ip filter OUTPUT oifname "eth0" tcp sport _PRIVOXY_PORT_ ct state established counter accept||g' '/nftables.rules'
-fi
-
 ## Flusing ruleset and add missing route ##
 echo '[info] Flusing ruleset'
 nft flush ruleset
@@ -45,7 +10,6 @@ echo "[info] Added route $add_route"
 ## Apply rules ##
 echo '[info] Apply rules'
 nft -f /nftables.rules
-rm /nftables.rules
 
 ## Quick block test ##
-source /static-ubuntu/scripts-debug/quick_block_test.sh
+source /static-ubuntu/scripts-openvpn//quick_block_test.sh
