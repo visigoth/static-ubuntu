3f51b4bc4ec00837c47a4124d3c69f0f636dfa91
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 12:03:45 +0000

    Update set-variables.sh

--- scripts-set/set-variables.sh
+++ scripts-set/set-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-## Set runtime variables depending on whether app is installed ##
-echo '[info] Set runtime variables'
+## Check app is installed before performing tasks ##
+echo '[info] Set runtime variables and fix configs'
 
 if [[ -f "/app/launcher/index.html" ]]
 then
@@ -14,49 +14,61 @@ then
     DNS_PORT=${DNS_SERVER_PORT}
     # DoT port is fixed due to TLS protocol
     DOT_PORT=853
+    source /static-ubuntu/scripts-fix/fix-stubby.sh
 fi
 
 if [[ -f "/usr/sbin/danted" ]]
 then
     DANTE_PORT=${SOCKS_PROXY_PORT}
+    source /static-ubuntu/scripts-fix/fix-dante.sh
 fi
 
 if [[ -f "/usr/bin/tinyproxy" ]]
 then
     TINYPROXY_PORT=${HTTP_PROXY_PORT}
+    source /static-ubuntu/scripts-fix/fix-tinyproxy.sh
 fi
 
 if [[ -f "/usr/sbin/tor" ]]
 then
     TORSOCKS_PORT=${TOR_SOCKS_PORT}
+    source /static-ubuntu/scripts-fix/fix-torsocks.sh
     PRIVOXY_PORT=${TOR_HTTP_PORT}
+    source /static-ubuntu/scripts-fix/fix-privoxy.sh
 fi
 
 if [[ -f "/usr/bin/sabnzbdplus" ]]
 then
     SAB_PORT_A=${USENET_HTTP_PORT}
     SAB_PORT_B=${USENET_HTTPS_PORT}
+    source /static-ubuntu/scripts-fix/fix-sabnzbdplus.sh
 fi
 
 if [[ -f "/usr/bin/flood" ]]
 then
     FLOOD_IP=${SERVER_IP}
     FLOOD_PORT=${TORRENT_GUI_PORT}
+    source /static-ubuntu/scripts-fix/fix-flood.sh
+    source /static-ubuntu/scripts-fix/fix-transmission.sh
 fi
 
 if [[ -f "/app/nzbhydra2/package_info" ]]
 then
     HYDRA_PORT=${SEARCHER_GUI_PORT}
+    source /static-ubuntu/scripts-fix/fix-nzbhydra2.sh
 fi
 
 if [[ -f "/app/sonarr/package_info" ]]
 then
     SONARR_PORT=${PVR_TV_PORT}
+    source /static-ubuntu/scripts-fix/fix-sonarr.sh
 fi
 
 if [[ -f "/app/radarr/package_info" ]]
 then
     RADARR_PORT=${PVR_MOVIE_PORT}
+    source /static-ubuntu/scripts-fix/fix-radarr.sh
+    source /static-ubuntu/scripts-fix/fix-prowlarr.sh
 fi
 
 if [[ -f "/app/prowlarr/package_info" ]]
@@ -67,6 +79,7 @@ fi
 if [[ -f "/app/jackett/jackett" ]]
 then
     JACKETT_PORT=${TORZNAB_PORT}
+    source /static-ubuntu/scripts-fix/fix-jackett.sh
 fi
 
 if [[ -f "/usr/sbin/openvpn" ]]
