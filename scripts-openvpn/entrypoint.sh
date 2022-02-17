6c686326b97e8372ddf3f128cf66fc2727e4041d
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 14:12:26 +0000

    Update entrypoint.sh

--- scripts-openvpn/entrypoint.sh
+++ scripts-openvpn/entrypoint.sh
@@ -4,17 +4,20 @@
 if [[ -f "/config/openvpn/openvpn.ovpn" ]]
 then
     echo '[info] OpenVPN config file detected. Continue...'
+    
     # Disable health check #
-    echo '[info] Disabling healthcheck while openvpn is connecting'
-    touch /config/disable_healthcheck
+    echo ''
+    echo '[info] Disabling healthcheck while openvpn is connecting...'
+    touch /config/healthcheck-disable
+    echo '[info] Healthcheck disabled'
     
     # Initilise apps #
     echo ''
-    echo '[info] Initialising apps'
+    echo '[info] Initialisation started...'
     source /static-ubuntu/scripts-openvpn/initialise.sh
     echo '[info] Initialisation complete'
 
-    # Use stubby to check external IP #
+    # Run stubby and use it to check external ISP IP #
     echo ''
     echo "[info] Run stubby in background on port $DNS_PORT"
     stubby -g -C /config/stubby/stubby.yml
@@ -23,26 +26,28 @@ then
 
     # nftables #
     echo ''
-    echo '[info] Set up nftables rules'
+    echo '[info] Setting up nftables rules...'
     source /static-ubuntu/scripts-openvpn/nftables.sh
     echo '[info] All rules created'
 
     # OpenVPN #
     echo ''
-    echo "[info] Setting up OpenVPN tunnel"
+    echo "[info] Setting up OpenVPN tunnel..."
     source /static-ubuntu/scripts-openvpn/openvpn.sh
-    echo '[info] Done'
+    echo '[info] Tunnel created'
 
     # Enable health check #
-    echo '[info] Enabling healthcheck'
-    rm -f /config/disable_healthcheck
+    echo ''
+    echo '[info] Enabling healthcheck...'
+    rm -f /config/healthcheck-disable
+    echo '[info] Healthcheck enabled'
     
     # Run apps #
     echo ''
     echo "[info] Runing apps..."
-    touch /config/disable_error
+    touch /config/healthcheck-no-error
     source /static-ubuntu/scripts-openvpn/healthcheck.sh
-    rm -f /config/disable_error
+    rm -f /config/healthcheck-no-error
     echo "[info] All done"
 
     ### Periodically checking IP ###
