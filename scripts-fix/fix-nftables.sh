df2ec392f2f2add7e464e679e6553503792fc53a
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 17:43:39 +0000

    Update fix-nftables.sh

--- scripts-fix/fix-nftables.sh
+++ scripts-fix/fix-nftables.sh
@@ -22,7 +22,7 @@ ETH0_IP=${ETH0_IP:0:$ETH0_IPLEN}
 ETH0_NET="$ETH0_NET0$ETH0_RANGE"
 echo "[info] eth0 IP is $ETH0_IP in network $ETH0_NET"
 
-## Editing ruleset ##
+## Create nftabless.rules ##
 echo '[info] Editing base ruleset'
 rm -f /nftables.rules
 cp -f /static-ubuntu/etc/nftables.raw /nftables.rules
@@ -30,31 +30,3 @@ sed -i "s|_ETH0_NET_|$ETH0_NET|g" '/nftables.rules'
 sed -i "s|_HOST_NETWORK_|${HOST_NETWORK}|g" '/nftables.rules'
 sed -i "s|_OPENVPN_PROTO_|$OPENVPN_PROTO|g" '/nftables.rules'
 sed -i "s|_OPENVPN_PORT_|$OPENVPN_PORT|g" '/nftables.rules'
-
-## Run TOR+Privoxy depending on build ##
-if [[ -f "/usr/sbin/tor" ]]; then
-    echo '[info] Tor build detected. Editing tor + privoxy ruleset'
-    sed -i "s|_TORSOCKS_PORT_|$TORSOCKS_PORT|g" '/nftables.rules'
-    sed -i "s|_PRIVOXY_PORT_|$PRIVOXY_PORT|g" '/nftables.rules'
-else
-    echo '[info] Torless build detected. Removing tor + privoxy ruleset'
-    sed -i 's|add rule ip filter INPUT iifname "eth0" tcp dport _TORSOCKS_PORT_ ct state new,established counter accept||g' '/nftables.rules'
-    sed -i 's|add rule ip filter INPUT iifname "eth0" tcp dport _PRIVOXY_PORT_ ct state new,established counter accept||g' '/nftables.rules'
-    sed -i 's|add rule ip filter OUTPUT oifname "eth0" tcp sport _TORSOCKS_PORT_ ct state established counter accept||g' '/nftables.rules'
-    sed -i 's|add rule ip filter OUTPUT oifname "eth0" tcp sport _PRIVOXY_PORT_ ct state established counter accept||g' '/nftables.rules'
-fi
-
-## Flusing ruleset and add missing route ##
-echo '[info] Flusing ruleset'
-nft flush ruleset
-add_route="$(ip route | grep 'default')" ; add_route="$(sed "s|default|$HOST_NETWORK|g" <<< $add_route)"
-ip route add $add_route
-echo "[info] Added route $add_route"
-
-## Apply rules ##
-echo '[info] Apply rules'
-nft -f /nftables.rules
-rm /nftables.rules
-
-## Quick block test ##
-source /static-ubuntu/scripts-debug/quick_block_test.sh
