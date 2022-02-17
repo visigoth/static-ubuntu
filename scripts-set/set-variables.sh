b57e0ccafd521cc0e5601cfd144de31155366884
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 11:51:34 +0000

    Update set-variables.sh

--- scripts-set/set-variables.sh
+++ scripts-set/set-variables.sh
@@ -1,75 +1,76 @@
 #!/bin/bash
 
-### DNS and DoT ports are fixed ###
+## Set runtime variables depending on whether app is installed ##
 echo '[info] Set runtime variables'
 
-if [[ -f "" ]]
+if [[ -f "/app/launcher/index.html" ]]
 then
     LAUNCHER_PORT=${LAUNCHER_GUI_PORT}
     LAUNCHER_IP=${SERVER_IP}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/usr/bin/stubby" ]]
 then
     DNS_PORT=${DNS_SERVER_PORT}
+    # DoT port is fixed due to TLS protocol
+    DOT_PORT=853
 fi
 
-if [[ -f "" ]]
+if [[ -f "/usr/sbin/danted" ]]
 then
     DANTE_PORT=${SOCKS_PROXY_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/usr/bin/tinyproxy" ]]
 then
     TINYPROXY_PORT=${HTTP_PROXY_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/usr/sbin/tor" ]]
 then
     TORSOCKS_PORT=${TOR_SOCKS_PORT}
     PRIVOXY_PORT=${TOR_HTTP_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/usr/bin/sabnzbdplus" ]]
 then
     SAB_PORT_A=${USENET_HTTP_PORT}
     SAB_PORT_B=${USENET_HTTPS_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/usr/bin/flood" ]]
 then
     FLOOD_IP=${SERVER_IP}
     FLOOD_PORT=${TORRENT_GUI_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/app/nzbhydra2/package_info" ]]
 then
     HYDRA_PORT=${SEARCHER_GUI_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/app/sonarr/package_info" ]]
 then
     SONARR_PORT=${PVR_TV_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/app/radarr/package_info" ]]
 then
     RADARR_PORT=${PVR_MOVIE_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/app/prowlarr/package_info" ]]
 then
     PROWLARR_PORT=${INDEXER_PORT}
 fi
 
-if [[ -f "" ]]
+if [[ -f "/app/jackett/jackett" ]]
 then
     JACKETT_PORT=${TORZNAB_PORT}
 fi
 
-# DoT port is fixed due to TLS protocol
-DOT_PORT=853
-
-### static scripts ###
-source /static/scripts/set_variables_ovpn_port_proto.sh
-source /static/scripts/set_variables_eth0.sh
+if [[ -f "/usr/sbin/openvpn" ]]
+then
+    source /static-ubuntu/scripts-set/set_variables_ovpn_port_proto.sh
+    source /static-ubuntu/scripts-set/set_variables_eth0.sh
+fi
