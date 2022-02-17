5e4bf1bdff89eff2dd728e936e19b5ad2f1fae2d
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 12:38:39 +0000

    Update fix-prowlarr.sh

--- scripts-fix/fix-prowlarr.sh
+++ scripts-fix/fix-prowlarr.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PROWLARR_PORT=${INDEXER_PORT}
+sed -i "s|:9696|:$PROWLARR_PORT|g" '/app/launcher/index.html'
+
 mkdir -p /config/prowlarr \
     && cp -n /static-ubuntu/etc/prowlarr.xml /config/prowlarr/config.xml
 sed -i "s|9696|$PROWLARR_PORT|g" '/config/prowlarr/config.xml'
