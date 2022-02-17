5bd677bae2649442d603e0e6bef4d8e1e546239d
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 14:29:51 +0000

    Update fix-dante.sh

--- scripts-fix/fix-dante.sh
+++ scripts-fix/fix-dante.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 DANTE_PORT=${SOCKS_PROXY_PORT}
+sed -i "s|_DANTE_PORT_|$DANTE_PORT|g" '/nftables.rules'
+
 mkdir -p /config/dante \
     && cp -n /static-ubuntu/etc/danted.conf /config/dante/
 sed -i "s|port=1080|port=$DANTE_PORT|g" '/config/dante/danted.conf'
