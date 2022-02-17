1d142c63c34d14811dfda886752bca56ac9e0ffd
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 11:21:15 +0000

    Update fix-config-nftables.sh

--- scripts-fix-config/fix-config-nftables.sh
+++ scripts-fix-config/fix-config-nftables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-### Editing ruleset ###
+## Editing ruleset ##
 echo '[info] Editing base ruleset'
 rm -f /nftables.rules
 cp /ruleset.nft /nftables.rules
@@ -12,7 +12,7 @@ sed -i "s|_DNS_PORT_|$DNS_PORT|g" '/nftables.rules'
 sed -i "s|_DANTE_PORT_|$DANTE_PORT|g" '/nftables.rules'
 sed -i "s|_TINYPROXY_PORT_|$TINYPROXY_PORT|g" '/nftables.rules'
 
-### Run TOR+Privoxy depending on build ###
+## Run TOR+Privoxy depending on build ##
 if [[ -f "/usr/sbin/tor" ]]; then
     echo '[info] Tor build detected. Editing tor + privoxy ruleset'
     sed -i "s|_TORSOCKS_PORT_|$TORSOCKS_PORT|g" '/nftables.rules'
@@ -25,14 +25,17 @@ else
     sed -i 's|add rule ip filter OUTPUT oifname "eth0" tcp sport _PRIVOXY_PORT_ ct state established counter accept||g' '/nftables.rules'
 fi
 
-### Flusing ruleset and add missing route ###
+## Flusing ruleset and add missing route ##
 echo '[info] Flusing ruleset'
 nft flush ruleset
 add_route="$(ip route | grep 'default')" ; add_route="$(sed "s|default|$HOST_NETWORK|g" <<< $add_route)"
 ip route add $add_route
 echo "[info] Added route $add_route"
 
-### Apply rules ###
+## Apply rules ##
 echo '[info] Apply rules'
 nft -f /nftables.rules
 rm /nftables.rules
+
+## Quick block test ##
+source /static-ubuntu/scripts-debug/quick_block_test.sh
