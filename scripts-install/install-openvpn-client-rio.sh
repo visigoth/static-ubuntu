bd076dc6ac48ab5dc24c84284977f906db1e1472
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 19:35:49 +0000

    Update install-openvpn-client-rio.sh

--- scripts-install/install-openvpn-client-rio.sh
+++ scripts-install/install-openvpn-client-rio.sh
@@ -9,7 +9,22 @@ rm -Rf /static-ubuntu/deprecated
 # wipe openvpn etc config
 rm -Rf /etc/openvpn
 
-# Set permission
+# dup mono binary
+cp /usr/bin/mono /usr/bin/mono-sonarr \
+    && chmod +x /usr/bin/mono-sonarr
+
+# dup python3 binary
+cp /usr/bin/python3 /usr/bin/python3-launcher \
+    && chmod +x /usr/bin/python3-launcher
+
+# dup python2 binary
+cp /usr/bin/python2 /usr/bin/python2-launcher \
+    && chmod +x /usr/bin/python2-launcher
+
+# chmod scripts
+chmod +x /app/radarr/Radarr
+chmod +x /app/prowlarr/Prowlarr
+chmod +x /*.sh
 chmod +x /static-ubuntu/scripts-debug/*.sh
 chmod +x /static-ubuntu/scripts-fix/*.sh
 chmod +x /static-ubuntu/scripts-install/*.sh
