2f20e308bdc7dd25eeb985a169a0ce105e70a5d7
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 21:56:32 +0000

    Update install-openvpn-client-rio.sh

--- scripts-install/install-openvpn-client-rio.sh
+++ scripts-install/install-openvpn-client-rio.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-## Rename script folders ##
-mv /testdasi /static-ubuntu
+## Make copy of static folder ##
+cp -rf /testdasi /static-ubuntu
 
-# Selectively delete redundant files
+# Selectively delete redundant files #
 rm -Rf /static-ubuntu/deprecated
 
-# wipe openvpn etc config
+# wipe openvpn etc config #
 rm -Rf /etc/openvpn
 
-# dup mono binary
+# dup mono binary #
 cp /usr/bin/mono /usr/bin/mono-sonarr \
     && chmod +x /usr/bin/mono-sonarr
 
-# dup python3 binary
+# dup python3 binary #
 cp /usr/bin/python3 /usr/bin/python3-launcher \
     && chmod +x /usr/bin/python3-launcher
 
-# dup python2 binary
+# dup python2 binary #
 cp /usr/bin/python2 /usr/bin/python2-launcher \
     && chmod +x /usr/bin/python2-launcher
 
-# chmod scripts
+# chmod scripts #
 chmod +x /app/radarr/Radarr
 chmod +x /app/prowlarr/Prowlarr
 chmod +x /*.sh
