173d3bc284af0f0fa8c3a99ab27228fbe68d26e5
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-17 12:37:36 +0000

    Update fix-launcher.sh

--- scripts-fix/fix-launcher.sh
+++ scripts-fix/fix-launcher.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+LAUNCHER_PORT=${LAUNCHER_GUI_PORT}
+LAUNCHER_IP=${SERVER_IP}
+
 cp -f /static-ubuntu/etc/index.html /app/launcher/
 sed -i "s|192.168.1.1|$LAUNCHER_IP|g" '/app/launcher/index.html'
 sed -i "s|server 8000|server $LAUNCHER_PORT|g" '/app/launcher/launcher-python3.sh'
