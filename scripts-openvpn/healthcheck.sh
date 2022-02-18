bb213efbaafc458fee1b394001992b8f5c12ab4b
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-18 09:00:39 +0000

    Update healthcheck.sh

--- scripts-openvpn/healthcheck.sh
+++ scripts-openvpn/healthcheck.sh
@@ -25,7 +25,13 @@ else
     then
         if [[ -f "/usr/bin/stubby" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-stubby-at-$(date "+%d.%m.%Y_%T")"
+            fi
             stubby -g -C /config/stubby/stubby.yml
         fi
     fi
@@ -35,7 +41,13 @@ else
     then
         if [[ -f "/usr/sbin/danted" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-danted-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run danted in background on port $DANTE_PORT"
             danted -D -f /config/dante/danted.conf
         fi
@@ -46,7 +58,13 @@ else
     then
         if [[ -f "/usr/bin/tinyproxy" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-tinyproxy-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run tinyproxy in background with no log on port $TINYPROXY_PORT"
             tinyproxy -c /config/tinyproxy/tinyproxy.conf
         fi
@@ -57,7 +75,13 @@ else
         pidlist=$(pidof tor)
         if [ -z "$pidlist" ]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-tor-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run tor in background on port $TORSOCKS_PORT"
             tor --hush --runasdaemon 1 -f /config/tor/torrc
         fi
@@ -68,7 +92,13 @@ else
         pidlist=$(pidof privoxy)
         if [ -z "$pidlist" ]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-privoxy-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run privoxy in background on port $PRIVOXY_PORT"
             privoxy /config/privoxy/config
         fi
@@ -79,7 +109,13 @@ else
     then
         if [[ -f "/usr/bin/sabnzbdplus" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-sabnzbdplus-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run sabnzbdplus in background on HTTP port $SAB_PORT_A and HTTPS port $SAB_PORT_B"
             sabnzbdplus --daemon --config-file /config/sabnzbdplus/sabnzbdplus.ini --pidfile /config/sabnzbdplus/sabnzbd.pid
         fi
@@ -90,7 +126,13 @@ else
     then
         if [[ -f "/app/nzbhydra2/package_info" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-nzbhydra2-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run nzbhydra2 in background on port $HYDRA_PORT"
             /app/nzbhydra2/bin/nzbhydra2 --daemon --nobrowser --java /usr/lib/jvm/java-11-openjdk-amd64/bin/java --datafolder /config/nzbhydra2 --pidfile /config/nzbhydra2/nzbhydra2.pid
         fi
@@ -101,7 +143,13 @@ else
     then
         if [[ -f "/usr/bin/transmission-daemon" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-transmission-at-$(date "+%d.%m.%Y_%T")"
+            fi
             TUN0_IP=$(ip addr show tun0 | grep inet | awk '{print $2}' | cut -d'/' -f 1)
             if [ -z $TUN0_IP ]; then
                 echo "[CRITICAL] Tunnel not found. Will not start transmission"
@@ -118,7 +166,13 @@ else
     then
         if [[ -f "/usr/bin/flood" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-flood-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run flood in background at $FLOOD_IP:$FLOOD_PORT"
             start-stop-daemon --start --background --name flood --chdir /usr/bin --exec /usr/bin/flood -- --rundir=/config/flood --host=${SERVER_IP} --port=${TORRENT_GUI_PORT}
         fi
@@ -129,7 +183,13 @@ else
     then
         if [[ -f "/app/jackett/jackett" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+                then
+                    echo ''
+                else
+                    crashed=$(( $crashed + 1 ))
+                    touch "/config/healthcheck-failure-jackett-at-$(date "+%d.%m.%Y_%T")"
+                fi
             echo "[info] Run jackett in background on port $JACKETT_PORT"
             start-stop-daemon --start --background --chuid nobody --name jackett --chdir /app/jackett --exec /app/jackett/jackett -- --DataFolder=/config/jackett
         fi
@@ -140,7 +200,13 @@ else
     then
         if [[ -f "/app/sonarr/package_info" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-sonarr-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run sonarr in background on port $SONARR_PORT"
             start-stop-daemon --start --background --name sonarr --chdir /app/sonarr/bin --exec /usr/bin/mono-sonarr -- --debug Sonarr.exe -nobrowser -data=/config/sonarr
         fi
@@ -151,7 +217,13 @@ else
     then
         if [[ -f "/app/radarr/package_info" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-radarr-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run radarr in background on port $RADARR_PORT"
             start-stop-daemon --start --background --name radarr --chdir /app/radarr/bin --exec /app/radarr/bin/Radarr -- -nobrowser -data=/config/radarr
         fi
@@ -162,7 +234,13 @@ else
     then
         if [[ -f "/app/prowlarr/package_info" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-prowlarr-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run prowlarr in background on port $PROWLARR_PORT"
             start-stop-daemon --start --background --name prowlarr --chdir /app/prowlarr/bin --exec /app/prowlarr/bin/Prowlarr -- -nobrowser -data=/config/prowlarr
         fi
@@ -173,7 +251,13 @@ else
     then
         if [[ -f "/app/launcher/index.html" ]]
         then
-            crashed=$(( $crashed + 1 ))
+            if [[ -f "/config/healthcheck-no-error" ]]
+            then
+                echo ''
+            else
+                crashed=$(( $crashed + 1 ))
+                touch "/config/healthcheck-failure-launcher-at-$(date "+%d.%m.%Y_%T")"
+            fi
             echo "[info] Run WebUI launcher in background at $LAUNCHER_IP:$LAUNCHER_PORT"
             start-stop-daemon --start --background --name launcher --chdir /app/launcher --exec /app/launcher/launcher-python3.sh
         fi
@@ -185,12 +269,11 @@ else
     # No error if healthcheck-no-error is set #
     if [[ -f "/config/healthcheck-no-error" ]]
     then
-        echo ''
+        echo '[info] Last healthcheck failed, check config folder for more info'
     else
         # Return exit code for healthcheck #
         if (( $crashed > 0 ))
         then
-            touch "/config/healthcheck-failure-at-$(date "+%d.%m.%Y_%T")"
             exit 1
         else
             exit 0
