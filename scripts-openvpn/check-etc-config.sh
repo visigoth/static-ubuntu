7f57d2dde5be903b981c54c999eab6130e190563
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-20 11:45:13 +0000

    Update check-etc-config.sh

--- scripts-openvpn/check-etc-config.sh
+++ scripts-openvpn/check-etc-config.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-## Only run process if ovpn config found ##
+## Check ovpn in /etc ##
 if [[ -f "/etc/openvpn/openvpn.ovpn" ]]
 then
+    # Check ovpn in /config #
     if [[ -f "/config/openvpn/openvpn.ovpn" ]]
     then
         echo '[warn] OpenVPN config file detected in both /etc and /config'
     else
         echo '[warn] OpenVPN config file detected in /etc/openvpn'
-        echo '[warn] Please remap container /config to a host folder and place openvpn.ovpn under [host folder]/openvpn'
+        echo '[warn] Please remap container /config to a host folder and place openvpn config files under [host folder]/openvpn'
         echo '[info] Copying config from /etc to /config'
         cp -rn /etc/openvpn /config/
         echo '[info] Complete'
