b650425ab6c861c7aafc202136f01136fb01b8fa
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-20 10:37:52 +0000

    Update install-openvpn-client-aio-base.sh

--- scripts-install/install-openvpn-client-aio-base.sh
+++ scripts-install/install-openvpn-client-aio-base.sh
@@ -2,6 +2,3 @@
 
 ## Run required install scripts. More complex dependencies run first.##
 source /testdasi/scripts-install/install-openvpn-plus.sh
-
-## wipe openvpn etc config ##
-rm -Rf /etc/openvpn
