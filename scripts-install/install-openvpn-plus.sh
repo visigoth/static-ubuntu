e52d99d656222a069c4794a906787352039b6dfd
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-20 10:17:50 +0000

    Update install-openvpn-plus.sh

--- scripts-install/install-openvpn-plus.sh
+++ scripts-install/install-openvpn-plus.sh
@@ -17,15 +17,18 @@ echo "$(date "+%d.%m.%Y %T") Added nftables version ${NFT_VERSION}" >> /build.in
 ## stubby ##
 apt -y install stubby
 STUBBY_VERSION=$(stubby -V)
+systemctl disable stubby
 echo "$(date "+%d.%m.%Y %T") Added stubby version ${STUBBY_VERSION}" >> /build.info
 
 ## dante-server ##
 apt -y install dante-server
+systemctl disable danted
 DANTED_VERSION=$(danted -v | cut -d' ' -f 2 | cut -d'v' -f 2 | cut -c'1-5')
 echo "$(date "+%d.%m.%Y %T") Added dante-server version ${DANTED_VERSION}" >> /build.info
 
 ## tinyproxy ##
 apt -y install tinyproxy
+systemctl disable tinyproxy
 TINYPROXY_VERSION=$(tinyproxy -v | cut -d' ' -f 2)
 echo "$(date "+%d.%m.%Y %T") Added tinyproxy version ${TINYPROXY_VERSION}" >> /build.info
 
