17d68fb8c2c60fdcf6111fc763640cc5f79b8b20
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-20 10:40:27 +0000

    Create install-openvpn-client-aio.sh

---
+++ scripts-install/install-openvpn-client-aio.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## add tor and privoxy depending on torless build opt ##
+if [[ ${BUILD_OPT} =~ "torless" ]]
+then
+    echo "$(date "+%d.%m.%Y %T") Skip torsocks and privoxy due to build option ${BUILD_OPT}" >> /build.info
+else
+    source /testdasi/scripts-install/install-tor.sh
+fi
+
+## Make copy of static folder ##
+cp -rf /testdasi /static-ubuntu
+
+# Selectively delete redundant files #
+rm -Rf /static-ubuntu/deprecated
+
+## chmod scripts ##
+chmod +x /*.sh
+chmod +x /static-ubuntu/scripts-debug/*.sh
+chmod +x /static-ubuntu/scripts-fix/*.sh
+chmod +x /static-ubuntu/scripts-install/*.sh
+chmod +x /static-ubuntu/scripts-openvpn/*.sh
