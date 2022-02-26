291e088f8955a83de7969c68fd7c9e8c094e7c08
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-26 20:16:40 +0000

    Create install-pia-script.sh

---
+++ scripts-install/install-pia-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Install dependencies ##
+apt -y update \
+    && apt -y install curl jq unzip
+
+## Install launcher ##
+rm -rf /app/pia-script \
+    && mkdir -p /temp \
+    && cd /temp \
+    && curl -L "https://github.com/pia-foss/manual-connections/archive/refs/heads/master.zip" -o /temp/pia.zip \
+    && unzip /temp/pia.zip \
+    && mkdir -p /app \
+    && mv /temp/manual-connections-master /app/pia-script \
+    && chmod +x /app/pia-script/*.sh \
+    && chmod +x /app/pia-script/openvpn_config/*.sh \
+    && rm -f /temp/pia.zip
+
+## Set build info ##
+echo "$(date "+%d.%m.%Y %T") Added PIA manual connections scripts" >> /build.info
+
+## Clean up ##
+apt -y autoremove \
+    && apt -y autoclean \
+    && apt -y clean \
+    && rm -rf /tmp/* /var/tmp/* /var/lib/apt/lists/*
