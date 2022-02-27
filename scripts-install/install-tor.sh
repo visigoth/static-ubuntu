189f5db086de94ea04d52acbdd763baee1625c8d
testdasi <51292604+testdasi@users.noreply.github.com>
2022-02-27 10:41:23 +0000

    Update install-tor.sh

--- scripts-install/install-tor.sh
+++ scripts-install/install-tor.sh
@@ -5,13 +5,15 @@ apt -y update
 
 ## torsocks ##
 apt -y install torsocks
-systemctl disable tor
+systemctl disable tor \
+    && rm -Rf /etc/tor/*
 TOR_VERSION=$(tor --version | cut -d' ' -f 3 | cut -c'1-7')
 echo "$(date "+%d.%m.%Y %T") Added torsocks version ${TOR_VERSION}" >> /build.info
 
 ## privoxy ##
 apt -y install privoxy
-systemctl disable privoxy
+systemctl disable privoxy \
+    && rm -Rf /etc/privoxy/*
 PRIVOXY_VERSION=$(privoxy --version | cut -d' ' -f 3)
 echo "$(date "+%d.%m.%Y %T") Added privoxy version ${PRIVOXY_VERSION}" >> /build.info
 
