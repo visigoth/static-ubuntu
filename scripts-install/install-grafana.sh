3ad05e2970dee1ccb08823c4261313192ddf0c38
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-10 18:23:17 +0000

    Update install-grafana.sh

--- scripts-install/install-grafana.sh
+++ scripts-install/install-grafana.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-## Fix locales and tzdata to prevent tzdata stopping installation ##
-apt -y update \
-    && apt -y install locales tzdata
-locale-gen 'en_GB.UTF-8' \
-    && dpkg-reconfigure --frontend=noninteractive locales
-ln -snf /usr/share/zoneinfo/Europe/London /etc/localtime \
-    && echo 'Europe/London' > /etc/timezone \
-    && dpkg-reconfigure --frontend=noninteractive tzdata
-
 ## Install dependencies ##
 apt -y install gnupg gnupg1 gnupg2 dirmngr ca-certificates apt-transport-https software-properties-common
 
