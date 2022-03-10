8a22ab874d179d70bfcab4025a2925831ae40228
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-10 17:23:21 +0000

    Update install-grafana-unraid-stack-base.sh

--- scripts-install/install-grafana-unraid-stack-base.sh
+++ scripts-install/install-grafana-unraid-stack-base.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 
+source /testdasi/scripts-install/install-grafana.sh
+
 ## Fix locales and tzdata to prevent tzdata stopping installation ##
-apt -y update \
-    && apt -y install locales tzdata
-locale-gen 'en_GB.UTF-8' \
-    && dpkg-reconfigure --frontend=noninteractive locales
-ln -snf /usr/share/zoneinfo/Europe/London /etc/localtime \
-    && echo 'Europe/London' > /etc/timezone \
-    && dpkg-reconfigure --frontend=noninteractive tzdata
+#apt -y update \
+#    && apt -y install locales tzdata
+#locale-gen 'en_GB.UTF-8' \
+#    && dpkg-reconfigure --frontend=noninteractive locales
+#ln -snf /usr/share/zoneinfo/Europe/London /etc/localtime \
+#    && echo 'Europe/London' > /etc/timezone \
+#    && dpkg-reconfigure --frontend=noninteractive tzdata
 
 ## Install dependencies ##
-apt -y install gnupg gnupg1 gnupg2 dirmngr ca-certificates apt-transport-https software-properties-common
+#apt -y install gnupg gnupg1 gnupg2 dirmngr ca-certificates apt-transport-https software-properties-common
 apt -y install lm-sensors smartmontools ipmitool
 
-# add grafana repo
-#wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
-curl -sOL "https://packages.grafana.com/gpg.key" | apt-key add -
-echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
 # add telegraf repo
 #wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
 curl -sOL "https://repos.influxdata.com/influxdb.key" | apt-key add -
 echo "deb https://repos.influxdata.com/debian buster stable" | tee /etc/apt/sources.list.d/influxdb.list
 
-## Fix locales and tzdata to prevent tzdata stopping installation ##
-apt -y install locales tzdata
-locale-gen 'en_GB.UTF-8' \
-    && dpkg-reconfigure --frontend=noninteractive locales
-ln -snf /usr/share/zoneinfo/Europe/London /etc/localtime \
-    && echo 'Europe/London' > /etc/timezone \
-    && dpkg-reconfigure --frontend=noninteractive tzdata
-
 # install grafana telegraf influxdb hddtemp
 apt-get -y update \
-    && apt-get -y install grafana telegraf influxdb hddtemp
+    && apt-get -y install telegraf influxdb hddtemp
 
 # install loki and promtail
 LOKI_RELEASE=$(curl -sX GET "https://api.github.com/repos/grafana/loki/releases/latest" | jq -r .tag_name)
