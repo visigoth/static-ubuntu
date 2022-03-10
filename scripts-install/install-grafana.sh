2a0320cafbdfe1f2fdd5e046a81f504655d5a53b
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-10 17:29:54 +0000

    Update install-grafana.sh

--- scripts-install/install-grafana.sh
+++ scripts-install/install-grafana.sh
@@ -13,17 +13,17 @@ ln -snf /usr/share/zoneinfo/Europe/London /etc/localtime \
 apt -y install gnupg gnupg1 gnupg2 dirmngr ca-certificates apt-transport-https software-properties-common
 
 ## Install grafana from repo ##
-#wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
 curl -sOL "https://packages.grafana.com/gpg.key" | apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | tee -a /etc/apt/sources.list.d/grafana.list
 apt-get -y update \
     && apt-get -y install grafana
-systemctl daemon-reload
-systemctl disable grafana-server
-
-## clean config ##
+systemctl daemon-reload \
+    && systemctl disable grafana-server
+# clean config #
 rm -f /etc/default/grafana-server \
     && touch /etc/default/grafana-server
+GRAFANA_VERSION=$(grafana-server -v | cut -d' ' -f 2)
+echo "$(date "+%d.%m.%Y %T") Added grafana-server version ${GRAFANA_VERSION}" >> /build.info
 
 ## Clean up ##
 apt -y autoremove \
