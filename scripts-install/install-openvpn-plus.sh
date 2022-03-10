c4e90e6c63b41b31949d4ac93aef03c7e4cfbbdc
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-10 19:05:12 +0000

    Update install-openvpn-plus.sh

--- scripts-install/install-openvpn-plus.sh
+++ scripts-install/install-openvpn-plus.sh
@@ -3,10 +3,11 @@
 ## Install dependencies ##
 apt -y update \
     && apt -y install dnsutils sipcalc
+mkdir -p /etc/init.d.disabled
 
 ## openvpn ##
 apt -y install openvpn
-systemctl disable openvpn
+mv /etc/init.d/openvpn /etc/init.d.disabled/
 # wipe openvpn etc default config to prevent dup #
 rm -Rf /etc/openvpn
 OPENVPN_VERSION=$(openvpn --version | grep 'linux-gnu' | cut -d' ' -f 2)
@@ -20,20 +21,19 @@ echo "$(date "+%d.%m.%Y %T") Added nftables version ${NFT_VERSION}" >> /build.in
 ## stubby ##
 apt -y install stubby
 STUBBY_VERSION=$(stubby -V)
-systemctl disable stubby \
-    && rm -Rf /etc/stubby/*
+rm -Rf /etc/stubby/*
 echo "$(date "+%d.%m.%Y %T") Added stubby version ${STUBBY_VERSION}" >> /build.info
 
 ## dante-server ##
 apt -y install dante-server
-systemctl disable danted \
+mv /etc/init.d/danted /etc/init.d.disabled/ \
     && rm -f /etc/danted.conf
 DANTED_VERSION=$(danted -v | cut -d' ' -f 2 | cut -d'v' -f 2 | cut -c'1-5')
 echo "$(date "+%d.%m.%Y %T") Added dante-server version ${DANTED_VERSION}" >> /build.info
 
 ## tinyproxy ##
 apt -y install tinyproxy
-systemctl disable tinyproxy \
+mv /etc/init.d/tinyproxy /etc/init.d.disabled/ \
     && rm -Rf /etc/tinyproxy/*
 TINYPROXY_VERSION=$(tinyproxy -v | cut -d' ' -f 2)
 echo "$(date "+%d.%m.%Y %T") Added tinyproxy version ${TINYPROXY_VERSION}" >> /build.info
