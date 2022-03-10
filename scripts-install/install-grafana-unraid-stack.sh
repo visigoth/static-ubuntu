bb56276a5796092a68afdad3fac79242908ff36e
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-10 21:27:16 +0000

    Update install-grafana-unraid-stack.sh

--- scripts-install/install-grafana-unraid-stack.sh
+++ scripts-install/install-grafana-unraid-stack.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# replace grafana service
+## Make copy of static folder ##
+mkdir -p /static-ubuntu
+cp -rf /testdasi/grafana-unraid-stack /static-ubuntu/
+
+## Replace service ##
 rm -f /etc/init.d/grafana-server \
-    && cp /temp/grafana-server /etc/init.d/ \
-    && chmod +x /etc/init.d/grafana-server
+    && rm -f /etc/init.d/influxdb \
+    && rm -f /etc/init.d/telegraf
+cp /static-ubuntu/grafana-unraid-stack/init.d/* /etc/init.d/
 
-# replace influxdb service
-rm -f /etc/init.d/influxdb \
-    && cp /temp/influxdb /etc/init.d/ \
-    && chmod +x /etc/init.d/influxdb
-
-# replace telegraf service
-rm -f /etc/init.d/telegraf \
-    && cp /temp/telegraf /etc/init.d/ \
-    && chmod +x /etc/init.d/telegraf
-
-# chmod scripts
+## chmod ##
 chmod +x /*.sh
+chmod +x /etc/init.d/*
