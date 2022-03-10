7c59f52d0eb24232336797009cbc00ee57b2b402
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-10 18:35:23 +0000

    Update install-grafana-unraid-stack-base.sh

--- scripts-install/install-grafana-unraid-stack-base.sh
+++ scripts-install/install-grafana-unraid-stack-base.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-## Install dependencies ##
-#apt -y update \
-#    && apt -y install lm-sensors smartmontools ipmitool hddtemp
-#rm -f /etc/hddtemp.db
+## Fix locales and tzdata to prevent tzdata stopping installation ##
+apt -y update \
+    && apt -y install locales tzdata
+locale-gen 'en_GB.UTF-8' \
+    && dpkg-reconfigure --frontend=noninteractive locales
+ln -snf /usr/share/zoneinfo/Europe/London /etc/localtime \
+    && echo 'Europe/London' > /etc/timezone \
+    && dpkg-reconfigure --frontend=noninteractive tzdata
 
-apt -y update
+## Install additional tools ##
+apt -y install lm-sensors smartmontools ipmitool
+
+## Install hddtemp ##
+apt -y install hddtemp
+rm -f /etc/hddtemp.db
+HDDTEMP_VERSION=$(hddtemp -v | cut -d' ' -f 3)
+echo "$(date "+%d.%m.%Y %T") Added hddtemp version ${LOKI_VER}" >> /build.info
+
 source /testdasi/scripts-install/install-grafana.sh
-apt -y update
 source /testdasi/scripts-install/install-influxdb-telegraf.sh
-apt -y update
 source /testdasi/scripts-install/install-loki-promtail.sh
 
 # clean up
