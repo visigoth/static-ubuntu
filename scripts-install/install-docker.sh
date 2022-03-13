5d49bb99dcc126c25e641e06bfa7c54b371d1066
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-13 15:36:36 +0000

    Update install-docker.sh

--- scripts-install/install-docker.sh
+++ scripts-install/install-docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo apt update -y \
-    && sudo apt upgrade -y \
-    && sudo apt full-upgrade -y \
-    && sudo apt autoremove -y
+apt update -y \
+    && apt upgrade -y \
+    && apt full-upgrade -y \
+    && apt autoremove -y \
+    && apt install -y apt-utils
+
+curl -sSL https://get.docker.com | sh
