83e805f33b714f5bd1bca145a8386513b4bedb01
testdasi <51292604+testdasi@users.noreply.github.com>
2022-03-14 22:26:58 +0000

    Update index-aio-plus.html, index-rio.html, and 2 more files...

--- openvpn-client/scripts-fix/fix-launcher.sh
+++ openvpn-client/scripts-fix/fix-launcher.sh
@@ -3,7 +3,18 @@
 LAUNCHER_PORT=${LAUNCHER_GUI_PORT}
 LAUNCHER_IP=${SERVER_IP}
 
-cp -f /static-ubuntu/openvpn-client/etc/index.html /app/launcher/
+# Pick index.html #
+if [[ -f "/app/radarr/package_info" ]]
+then
+    cp -f /static-ubuntu/openvpn-client/etc/index-rio.html /app/launcher/index.html
+elif [[ -f "/app/nzbhydra2/package_info" ]]
+then
+    cp -f /static-ubuntu/openvpn-client/etc/index-aio-plus.html /app/launcher/index.html
+else
+    echo '[ERROR] No index.html selected due to unexpected installed packs.'
+fi
+
+# Fix configs #
 sed -i "s|192.168.1.1|$LAUNCHER_IP|g" '/app/launcher/index.html'
 sed -i "s|server 8000|server $LAUNCHER_PORT|g" '/app/launcher/launcher-python3.sh'
 sed -i "s|python3 |python3-launcher |g" '/app/launcher/launcher-python3.sh'
